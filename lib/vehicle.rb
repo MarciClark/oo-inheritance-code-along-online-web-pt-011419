bbea18b0b657001d671b81803ba7e6f2bce1cd01
Marci Clark <marcig612@gmail.com>
2019-02-16 21:32:31 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,11 @@
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(wheel_suze, wheel_number)
+    @wheel_size = wheel_size 
+    @wheel_number = whee;_number 
+  end 
+  
+  
 end
